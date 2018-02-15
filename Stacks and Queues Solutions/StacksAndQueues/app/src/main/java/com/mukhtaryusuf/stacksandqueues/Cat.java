54e02c7b249ff8457defc37e282cc52f814d50db
MukhtarYusuf <mukhtar.yusuf1989@gmail.com>
2018-02-14 19:04:29 -0500

    Created Cat Class

---
+++ Stacks and Queues Solutions/StacksAndQueues/app/src/main/java/com/mukhtaryusuf/stacksandqueues/Cat.java
@@ -0,0 +1,13 @@
+package com.mukhtaryusuf.stacksandqueues;
+
+/**
+ * Created by mukhtaryusuf on 2/7/18.
+ */
+
+public class Cat extends Animal {
+    public Cat(String name){
+        this.name = name;
+        this.type = "Cat";
+        this.tStamp = Integer.MAX_VALUE;
+    }
+}
