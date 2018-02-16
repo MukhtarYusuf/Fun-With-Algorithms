1bff86e083847f6ae35e4fa6642614880e343a76
MukhtarYusuf <mukhtar.yusuf1989@gmail.com>
2018-02-16 01:43:37 -0500

    Added Dog Class

---
+++ Stacks and Queues Solutions/StacksAndQueues/app/src/main/java/com/mukhtaryusuf/stacksandqueues/Dog.java
@@ -0,0 +1,13 @@
+package com.mukhtaryusuf.stacksandqueues;
+
+/**
+ * Created by mukhtaryusuf on 2/7/18.
+ */
+
+public class Dog extends Animal {
+    public Dog(String name){
+        this.name = name;
+        this.type = "Dog";
+        this.tStamp = Integer.MAX_VALUE;
+    }
+}
