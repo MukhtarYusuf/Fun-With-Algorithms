d561532008be63ce9524264c2d7548e1a76a469b
MukhtarYusuf <mukhtar.yusuf1989@gmail.com>
2018-03-14 22:28:04 -0400

    Added solution to problem 1

--- Trees and Graphs Solutions/TreesandGraphs/app/src/main/java/com/mukhtaryusuf/treesandgraphs/MainActivity.java
+++ Trees and Graphs Solutions/TreesandGraphs/app/src/main/java/com/mukhtaryusuf/treesandgraphs/MainActivity.java
@@ -3,11 +3,32 @@ package com.mukhtaryusuf.treesandgraphs;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.LinkedList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /*
+    1. Write a method to check if a route exists between two nodes
+     */
+    public boolean existsRoute(GraphNode a, GraphNode b){
+        if( a == null || b == null)
+            return false;
+
+        LinkedList<GraphNode> q = new LinkedList<>();
+        q.addLast(a);
+        while(!q.isEmpty()){
+            GraphNode cur = q.removeFirst();
+            if(cur == b)
+                return true;
+            for(GraphNode graphNode : cur.children)
+                q.addLast(graphNode);
+        }
+        return false;
     }
 }
