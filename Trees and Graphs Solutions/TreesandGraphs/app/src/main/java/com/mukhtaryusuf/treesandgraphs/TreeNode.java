3090a82180dfdaca4e59ebe8c730337566eeca77
MukhtarYusuf <mukhtar.yusuf1989@gmail.com>
2018-03-18 23:17:47 -0400

    Added Tree Node Class

---
+++ Trees and Graphs Solutions/TreesandGraphs/app/src/main/java/com/mukhtaryusuf/treesandgraphs/TreeNode.java
@@ -0,0 +1,16 @@
+package com.mukhtaryusuf.treesandgraphs;
+
+/**
+ * Created by mukhtaryusuf on 3/14/18.
+ */
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode parent;
+
+    public TreeNode(int val){
+        this.val = val;
+    }
+}
