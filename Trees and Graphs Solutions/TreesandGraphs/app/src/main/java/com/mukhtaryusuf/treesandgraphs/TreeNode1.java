d9dec18564ccdf4e42f711d2939e1d58d15f7676
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2018-05-13 15:34:33 -0400

    Added TreeNode Implementation

---
+++ Trees and Graphs Solutions/TreesandGraphs/app/src/main/java/com/mukhtaryusuf/treesandgraphs/TreeNode1.java
@@ -0,0 +1,19 @@
+package com.mukhtaryusuf.treesandgraphs;
+
+import java.util.Random;
+
+/**
+ * Created by mukhtaryusuf on 5/10/18.
+ */
+
+public class TreeNode1 {
+    public int val;
+    public TreeNode1 left;
+    public TreeNode1 right;
+    private int size;
+
+    public TreeNode1(int val){
+        this.val = val;
+        this.size = 1;
+    }
+}
