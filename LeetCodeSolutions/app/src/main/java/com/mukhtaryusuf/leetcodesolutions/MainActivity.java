5382bb61ca477f08a827a7f213177f66883abc61
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2018-07-10 18:31:43 -0400

    Added Solution to Longest Substring Without Repeating Characters

--- LeetCodeSolutions/app/src/main/java/com/mukhtaryusuf/leetcodesolutions/MainActivity.java
+++ LeetCodeSolutions/app/src/main/java/com/mukhtaryusuf/leetcodesolutions/MainActivity.java
@@ -21,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     Solution 3: Sorting and Searching. Time: O(nlogn), Space: O(n)
     Solution 4: Sorting with Two Pointer Approach. Time: O(nlogn), Space: O(n)
      */
+
+    //Solution 2
     public int[] twoSum(int[] nums, int target) {
         int[] indices = new int[2];
         HashMap<Integer,Integer> hm = new HashMap<Integer,Integer>();
@@ -39,5 +41,42 @@ public class MainActivity extends AppCompatActivity {
         }
         //printAll(2);
         return indices;
+    }
+
+    /*
+    Problem: Given a String, find the length of the longest substring without repeating characters
+
+    Solution 1: Two pointer solution using HashMap. Time: O(n), Space: O(n)
+    Solution 2: Two pointer solution using Array of Constant Size. Time: O(n), Space: O(1)
+     */
+
+    //Solution 2
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0)
+            return 0;
+        int length = s.length();
+        if (length == 1)
+            return 1;
+        int longestLength = 0;
+        int tempLength = 0;
+        int tailPointer = 0;
+        boolean[] tracker = new boolean[256];
+        for (int i = 0; i < length; i++) {
+            char c = s.charAt(i);
+            if (tracker[c]) {//Duplicate found
+                while (tailPointer < i) {
+                    char ch = s.charAt(tailPointer);
+                    tracker[ch] = false;
+                    tailPointer++;
+                    tempLength--;
+                    if (ch == c)
+                        break;
+                }
+            }
+            tracker[c] = true;
+            tempLength++;
+            longestLength = Math.max(longestLength, tempLength);
+        }
+        return longestLength;
     }
 }
