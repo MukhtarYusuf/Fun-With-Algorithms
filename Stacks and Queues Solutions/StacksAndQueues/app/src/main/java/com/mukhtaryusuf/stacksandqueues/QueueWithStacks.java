6511879875d137ddc5d508962fed2391e6f40686
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2018-07-16 14:22:19 -0400

    Revert "Added remove() method to QueWithStacks Implementation"
    This reverts commit a130b0a4ef8d1059e28ce50a606b5b1162b45301.

--- Stacks and Queues Solutions/StacksAndQueues/app/src/main/java/com/mukhtaryusuf/stacksandqueues/QueueWithStacks.java
+++ Stacks and Queues Solutions/StacksAndQueues/app/src/main/java/com/mukhtaryusuf/stacksandqueues/QueueWithStacks.java
@@ -14,20 +14,6 @@ public class QueueWithStacks<T>{
     public void insert(T value){
         stack1.push(value);
     }
-
-    public T remove(){
-        T removedValue = null;
-        if(isEmpty())
-            return removedValue;
-        else if(stack2.isEmpty()){
-            while(!stack1.isEmpty())
-                stack2.push(stack1.pop());
-        }
-        removedValue = stack2.pop();
-
-        return removedValue;
-    }
-
     public boolean isEmpty(){
         return stack1.isEmpty() && stack2.isEmpty();
     }
