399381f98bac46b98e6f4430a9a437dafd783657
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2018-10-04 19:45:48 -0400

    Added solution to "Sorted Merge" Problem

--- Sorting and Searching Solutions/SortingAndSearching/app/src/main/java/com/mukhtaryusuf/sortingandsearching/MainActivity.java
+++ Sorting and Searching Solutions/SortingAndSearching/app/src/main/java/com/mukhtaryusuf/sortingandsearching/MainActivity.java
@@ -11,6 +11,27 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+    /*
+    Problem 1: Sorted Merge
+     */
+
+    //Solution: Two Pointer Solution from Ends of Arrays
+    public void merge(int[] a, int[] b, int length){
+        if(a == null || b == null)
+            return;
+        int index = a.length - 1;
+        int aPtr = length - 1;
+        int bPtr = b.length - 1;
+        while(aPtr >= 0 && bPtr >= 0){
+            if(a[aPtr] > b[bPtr])
+                a[index--] = a[aPtr--];
+            else
+                a[index--] = b[bPtr--];
+        }
+        //Process b if not done. No need to process a
+        while(bPtr >= 0)
+            a[index--] = b[bPtr--];
+    }
     }
     //Bubble Sort
     public void bubbleSort(int[] array){
