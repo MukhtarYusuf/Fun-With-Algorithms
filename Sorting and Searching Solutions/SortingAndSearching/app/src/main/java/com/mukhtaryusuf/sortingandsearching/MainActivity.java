7980440116069a13ce376cc9f257c64727562244
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2018-10-29 22:39:13 -0400

    Added swap() helper method

--- Sorting and Searching Solutions/SortingAndSearching/app/src/main/java/com/mukhtaryusuf/sortingandsearching/MainActivity.java
+++ Sorting and Searching Solutions/SortingAndSearching/app/src/main/java/com/mukhtaryusuf/sortingandsearching/MainActivity.java
@@ -267,4 +267,9 @@ public class MainActivity extends AppCompatActivity {
         return tail;
     }
 
+    public void swap(int[] array, int index1, int index2){
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
 }
