b2e05305dfb366bcaaecf68eec324710e7b49909
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2018-11-18 19:12:46 -0500

    Added TreeNode for solution to "Rank From Stream" Problem

---
+++ Sorting and Searching Solutions/SortingAndSearching/app/src/main/java/com/mukhtaryusuf/sortingandsearching/TreeNode.java
@@ -0,0 +1,13 @@
+package com.mukhtaryusuf.sortingandsearching;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    int leftSize;
+
+    public TreeNode(int v){
+        val = v;
+    }
+
+}
